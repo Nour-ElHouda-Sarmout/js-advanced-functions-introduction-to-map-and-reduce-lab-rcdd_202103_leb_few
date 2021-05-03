746c7ea6bc41f886b204edb8f2d1c39fa62652c2
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 09:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Your code here
+
+function mapToNegativize(sourceArray) {
+  let r = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(-1 * sourceArray[i]) // Unique work
+  }
+  return r
+}
+
+function mapToNoChange(sourceArray) {
+  let r = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(sourceArray[i]) // Unique work
+  }
+  return r
+}
+
+function mapToDouble(sourceArray) {
+  let r = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(2 * sourceArray[i]) // Unique work
+  }
+  return r
+}
+
+function mapToSquare(sourceArray) {
+  let r = []
+  for (let i = 0; i < sourceArray.length; i++ ) {
+    r.push(src[i] * sourceArray[i]) // Unique work
+  }
+  return r
+}
+
+
+function reduceToTotal(sourceArray, startingPoint){
+  let total = startingPoint
+  for(let num of sourceArray){
+   total = total + num
+  }
+  return total
+}
+
+function  reduceToAllTrue(sourceArray){
+  let value = true
+  for(let item of sourceArray){
+    if (item === false){
+        value = false
+    }
+  }
+  return value
+}
+
+function  reduceToAnyTrue(sourceArray){
+  let value = false
+  for(let item of sourceArray){
+    if (item === true){
+        value = true
+    }
+  }
+  return value
+}
